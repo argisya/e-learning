ff36b095c87b8f69b5043a6362eaf1ec152184d0
RizlaAzc <azchafahrezi@gmail.com>
2026-04-06 18:12:20 +0700

    Role

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Roles extends Model
+class Role extends Model
 {
     use HasFactory;
 
